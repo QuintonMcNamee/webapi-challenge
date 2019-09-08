2af52ad6ec8f7799ecb60f18f6c12f88900c5a99
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-09-08 16:36:32 -0400

    actionRouter.js completed

---
+++ routes/actionRouter.js
@@ -0,0 +1,92 @@
+const express = require('express');
+
+const actionDb = require('../data/helpers/actionModel.js');
+
+const projectDb = require('../data/helpers/projectModel.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  actionDb.get()
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+
+  projectDb.getProjectActions(id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.post('/:id', validateLength, (req, res) => {
+  const id = req.params.id;
+  const something = req.body;
+
+  actionDb.insert(something)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+
+  actionDb.get(id)
+    .then(response => {
+      actionDb.remove(response.id)
+        .then(response => {
+          res.status(200).json(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.put('/:id', validateLength, (req, res) => {
+  const id = req.params.id;
+  const something = req.body;
+
+  actionDb.get(id)
+    .then(response => {
+      actionDb.update(id, response)
+        .then(response => {
+          res.status(200).json(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+// custom middleware
+
+function validateLength(req, res, next) {
+  const description = req.body.description;
+
+  if(description.length <= 128) {
+    next();
+  }
+  else {
+    res.status(404).json({ message: "Error"});
+  };
+};
